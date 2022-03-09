b03a81781c2f01d0cb8275f6cd1057e2f11101ce
JoaoPontes98 <37784969+JoaoPontes98@users.noreply.github.com>
2022-03-09 13:51:36 -0400

    Fixed list.java

--- List.java
+++ List.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class List {
     private Date date;
     private String title;
     private String description;
     private ArrayList<Task> tasks;
-    
 
-    public Task(Date date, String title, String description) {
+    public List(Date date, String title, String description) {
         this.date = date;
         this.title = title;
         this.description = description;
